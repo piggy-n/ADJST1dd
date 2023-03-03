2c1491f0c3b60554fb68cf97c876399325fec213
lzn01 <408135242@qq.com>
2023-03-03 14:33:56 +0800

    zustand context demo

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -1,6 +1,7 @@
 import { createStore } from 'zustand';
 import { createContext } from 'react';
 import type { StoreApi } from 'zustand';
+import { devtools } from 'zustand/middleware';
 
 interface CounterState {
     count: number;
@@ -13,16 +14,19 @@ interface CounterAction {
 
 export type CounterStore = CounterState & CounterAction;
 
-export const createCounterStore = (): StoreApi<CounterStore> => createStore<CounterStore>(
-    (set) => ({
-        count: 0,
-        increase: () => {
-            set((state) => ({ count: state.count + 1 }));
-        },
-        decrease: () => {
-            set((state) => ({ count: state.count - 1 }));
-        },
-    }),
+export const createCounterStore = (): StoreApi<CounterStore> => createStore<CounterStore>()(
+    devtools(
+        (set, get) => ({
+            count: 0,
+            increase: () => {
+                set((state) => ({ count: state.count + 1 }));
+            },
+            decrease: () => {
+                set((state) => ({ count: state.count - 1 }));
+            },
+        }),
+        { name: 'CounterStore' },
+    ),
 );
 
 export const CounterContext = createContext<ReturnType<typeof createCounterStore>>(<ReturnType<typeof createCounterStore>>{});
