750e844fd9accfb5a7d971a26453828bfd12233c
lzn01 <408135242@qq.com>
2023-03-06 15:21:42 +0800

    PropsUpdater

--- src/kernel/Player/PropsUpdater.tsx
+++ src/kernel/Player/PropsUpdater.tsx
@@ -1,8 +1,8 @@
 import { PlayerContext } from '@/store/Player';
 import { useContext, useEffect } from 'react';
+import { isObject } from 'lodash';
 import type { FC } from 'react';
 import type { PlayerProps } from '@/index.d';
-import { isObject } from 'lodash';
 
 const PropsUpdater: FC<PlayerProps> = (
     {
@@ -13,7 +13,7 @@ const PropsUpdater: FC<PlayerProps> = (
     },
 ) => {
     const store = useContext(PlayerContext);
-    const { setUrlAndVideoType } = store.getState();
+    const { setUrlAndVideoType, setUrlAndVideoTypeByDeviceOpts } = store.getState();
 
     const { deviceId, urlPrefix, streamType = '1', channelType = '1' } = deviceOpts || {};
 
@@ -21,19 +21,14 @@ const PropsUpdater: FC<PlayerProps> = (
         () => {
             if (url) {
                 const isLive = /^ws:\/\/|^wss:\/\//.test(url);
-                setUrlAndVideoType(url, videoType ?? (isLive ? 'live' : 'record'));
+                return setUrlAndVideoType(url, videoType ?? (isLive ? 'live' : 'record'));
+            }
+
+            if (isObject(deviceOpts) && deviceId) {
+                setUrlAndVideoTypeByDeviceOpts(deviceOpts, videoType);
             }
         },
-        [url, videoType],
-    );
-
-    useEffect(
-        () => {
-            if (isObject(deviceOpts) && deviceId && !url) {
-
-            }
-        },
-        [deviceId, urlPrefix, streamType, channelType],
+        [url, videoType, deviceId, urlPrefix, streamType, channelType],
     );
 
     return null;
--- src/store/Player/action.ts
+++ src/store/Player/action.ts
@@ -1,9 +1,11 @@
+import { obtainDeviceStreamList } from '@/utils/methods/async/device';
 import type { StoreApi } from 'zustand';
-import type { VideoType } from '@/index.d';
+import type { VideoType, DeviceOpts } from '@/index.d';
 import type { PlayerStoreState } from '@/store/Player/state';
 
 export interface PlayerStoreAction {
     setUrlAndVideoType: (url: string, videoType: VideoType) => void;
+    setUrlAndVideoTypeByDeviceOpts: (deviceOpts: DeviceOpts, videoType?: VideoType) => void;
 }
 
 export const action = (
@@ -11,5 +13,15 @@ export const action = (
     get: StoreApi<PlayerStoreState & PlayerStoreAction>['getState'],
 ): PlayerStoreAction => ({
     setUrlAndVideoType: (url, videoType) => set({ url, videoType }),
+    setUrlAndVideoTypeByDeviceOpts: async (deviceOpts, videoType?: VideoType) => {
+        const { streamType, channelType } = deviceOpts;
+        const streamList = await obtainDeviceStreamList(deviceOpts);
+        const streamInfo = streamList.find(item => item.streamTypeCode === streamType && item.channelCode === channelType);
+        const streamUrl = streamInfo?.url ?? '';
 
+        set({
+            url: streamUrl,
+            videoType: videoType === 'stream-record' ? 'stream-record' : 'live',
+        });
+    },
 });
